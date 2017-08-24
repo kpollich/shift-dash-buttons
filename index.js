b9d8ddca24cb9fdf9c1156fe42f4c0fffca07f82
Jason Stoltzfus <jastoltz24@gmail.com>
2017-08-24 16:28:59 -0400

    Added identities configuration

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config({ silent: true })
 const axios = require('axios')
 const dashButton = require('node-dash-button')
 
-const macAddresses = process.env.MAC_ADDRESSES.split(',')
+const identities = JSON.parse(process.env.IDENTITIES)
+const macAddresses = Object.keys(identities)
 const dash = dashButton(macAddresses, null, null, 'all')
 
 console.log('Starting dash button script...')
@@ -12,7 +13,7 @@ dash.on('detected', (id) => {
   console.log('Dash button activated with ID:', id)
 
   axios.post(process.env.SLACK_ENDPOINT, {
-    text: 'Kyle has signed on - hello everyone!'
+    text: `${identities[id]} has signed on - hello everyone!`
   })
     .then(() => console.log('Successfully sent slack message'))
     .catch(error => console.error(error))
