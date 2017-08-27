26dcd6b907c3fdd92ffa57d80d172793bd468ad1
Jason Stoltzfus <jastoltz24@gmail.com>
2017-08-27 16:10:28 -0400

    Fixed sign on check (#4)
    * Added random messages
    
    * Fixed kyles bullshit
    
    * Fixed signon check

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dash.on('detected', (id) => {
   console.log('Dash button activated with ID:', id)
 
   // Anything before 12PM is considered a sign on - afterwards it's a sign out
-  const isSignOn = new Date().getHours < 12
+  const isSignOn = new Date().getHours() < 12
   const name = identities[id]
 
   const text = isSignOn ? getMessage(name) : `${name} is heading out. Seeya later!`
